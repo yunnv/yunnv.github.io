dbc000a930623aea66bccc4dab489a45066b298b
yunnv <156573376+yunnv@users.noreply.github.com>
2024-01-14 19:13:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Like u more <3";
+  question.inerHTML = "Like u more <3";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
 
 noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
+  const noBtnRect = noBtn.getBoundingClinetRect();
   const maxX = window.innerWidth - noBtnRect.width;
   const maxY = window.innerHeight - noBtnRect.height;
-  
+
   const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
-  
+  const randomY = Math.floor(Math.randpm() * maxY);
+
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
 });
