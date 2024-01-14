1b6a1839cb17de6c27b05a9dd1ced6b328f8a79c
yunnv <156573376+yunnv@users.noreply.github.com>
2024-01-14 19:33:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.inerHTML = "Like u more <3";
+  question.inerHTML = "Like u moreee";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
